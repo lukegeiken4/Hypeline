311d6f07d280254268ecd6dd3b439f3adfb65884
garret.meier <garret.meier@bunchball.com>
2015-11-21 13:19:35 -0600

    facebook is happening

--- api/controllers/AnalyzeController.js
+++ api/controllers/AnalyzeController.js
@@ -6,6 +6,21 @@
  */
 
 module.exports = {
-	
+    get_nugs: function(req,res){
+        var origin = req.params.origin || null;
+        var response = {};
+
+        switch(origin){
+            case "twitter":
+                sails.controllers.twitter.get_raw_nugs(req,res);
+                break;
+            case "facebook":
+                sails.controllers.facebook.get_raw_nugs(req,res);
+                break;
+            default:
+                response.error = "No module for "+origin;
+                break;
+        }
+    }
 };
 
