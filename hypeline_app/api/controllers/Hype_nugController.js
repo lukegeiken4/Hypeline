41a5d296cc0585ba052491f1ec2caf185eb9566a
garret.meier <garret.meier@bunchball.com>
2015-11-21 00:53:47 -0600

    added the nug

---
+++ hypeline_app/api/controllers/Hype_nugController.js
@@ -0,0 +1,11 @@
+/**
+ * Hype_nugController
+ *
+ * @description :: Server-side logic for managing hype_nugs
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
